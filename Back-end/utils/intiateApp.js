07ff44e5235c91851295761855604ed9e468574a
ahmed_mohsen <am6945@fayoum.edu.eg>
2025-04-26 03:38:48 +0300

    Add Socket.IO integration for real-time communication; update package.json and package-lock.json to include socket.io and socket.io-client; implement socket connection in Root component and create socket instance in a new socket file.

--- Back-end/utils/intiateApp.js
+++ Back-end/utils/intiateApp.js
@@ -1,17 +1,47 @@
+import http from "http";
+import { Server } from "socket.io";
 import { connectionDB } from "../DB/connection.js";
 import { globalResponse } from "./errorHandling.js";
-import {setupRoutes}from "../modules/route.index.js";
+import { setupRoutes } from "../modules/route.index.js";
+
 export const initiateApp = async (app, express) => {
   app.use(express.json());
   await connectionDB();
+
   app.get("/api", (req, res) => {
     res.send("Hello World");
   });
-  const PORT = process.env.PORT || 5000;
+
   setupRoutes(app);
   app.use(globalResponse);
   app.all("*", (req, res, next) =>
     res.status(404).json({ message: "404 Not Found URL" })
   );
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+  const PORT = process.env.PORT || 5000;
+
+  const server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+  });
+
+  app.set("io", io);
+
+  io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    socket.on("join", (data) => {
+      console.log("User joined room", data.room);
+      socket.join(data.room);
+    });
+    socket.on("disconnect", () => {
+      console.log("User disconnected", socket.id);
+    });
+  });
+
+  server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
--- Front-end/src/pages/Root.jsx
+++ Front-end/src/pages/Root.jsx
@@ -3,7 +3,25 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/root/MainNavigation";
 import { useNavigate } from "react-router-dom";
 import stars from "../../assets/stars.png";
+import socket from "../../socket";
+import { useEffect } from "react";
 function Root() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+    socket.emit("join", {
+      room: `${localStorage.getItem("username")}->uploads`,
+    });
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
   return (
     <div
       className="bg-cover bg-center h-screen animate-move-background"
